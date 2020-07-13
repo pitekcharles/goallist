334439fed5e1139b456e4fb34bba6752f0dd9205
Charles Pitek <charles.pitek@gmail.com>
2020-07-13 14:15:24 -0600

    added functionallity where when yesbutton is clicked it say congrats on the page

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,11 @@
 var yesButton = document.getElementById("yesButton");
 
+function goalAchieved () {
+    var h2 = document.createElement("h2");
+    h2.innerText = "Congratulations!";
+    document.getElementById("goalReached").appendChild(h2);
+}
+
 yesButton.addEventListener("click", function () {
-    console.log("test");
+    goalAchieved();
 }, false);

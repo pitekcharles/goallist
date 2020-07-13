d7f6b53287aa128fbf523f3513af6da775bb32d5
Charles Pitek <charles.pitek@gmail.com>
2020-07-13 14:39:12 -0600

    added functionallity to the no button

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,6 +9,18 @@ function goalAchieved () {
     noButton.remove();
 }
 
+function goalFailed () {
+    var h2 = document.createElement("h2");
+    h2.innerText = "You can do it! Tomorrow is another chance!";
+    document.getElementById("goalReached").appendChild(h2);
+    yesButton.remove();
+    noButton.remove();
+}
+
 yesButton.addEventListener("click", function () {
     goalAchieved();
+}, false);
+
+noButton.addEventListener("click", function () {
+    goalFailed();
 }, false);

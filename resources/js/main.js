4b36c60ae454c82c5d40d11633d335a934d3f8ca
Charles Pitek <charles.pitek@gmail.com>
2020-07-13 14:24:27 -0600

    improved yes button functionality

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,9 +1,12 @@
 var yesButton = document.getElementById("yesButton");
+var noButton = document.getElementById("noButton");
 
 function goalAchieved () {
     var h2 = document.createElement("h2");
-    h2.innerText = "Congratulations!";
+    h2.innerText = "Congratulations you achieved your goals today!";
     document.getElementById("goalReached").appendChild(h2);
+    yesButton.remove();
+    noButton.remove();
 }
 
 yesButton.addEventListener("click", function () {
